5f583c2152ccacca053806a4aab23d35aefb5f2d
Oleg <Oleg@OLEG-PC>
2022-07-01 23:59:31 +0300

    Добавьте файлы проекта.

---
+++ NetLibraryTest/AnyCalc.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace NetLibraryTest
+{
+    public class AnyCalc
+    {
+        /*
+        Library for calc area of cicle and triangle
+        Features: autodect figures, unit test 
+        */
+        public struct Figure // type figure for return result
+        {
+            public string type; // type of figure
+            public double area; // area
+            public string message; // details
+        }
+
+
+        public Figure AnyFigure(double a) // if we have 1 arguments
+        {
+           
+            double s = Math.PI * Math.Pow(a, 2);
+            Figure f = new Figure
+            {
+                type = "Cicle",
+                area = s,
+                message = "Cicle, Area: " + s
+            };
+            return f;
+
+        }
+        public Figure AnyFigure(double a, double b, double c) // if we have 3 arguments
+        {
+            double s = (a + b + c) / 2;
+
+            Figure f = new Figure
+            {
+                area = s
+            };
+
+
+            if (System.Math.Pow(c, 2) == System.Math.Pow(a, 2) + System.Math.Pow(b, 2)) //if right triangle
+            {
+                f.type = "Triangle";
+                f.message = f.type + ", area: " + s;
+            }
+            else
+            {
+                f.type = "Right triangle";
+                f.message = f.type + ", area: " + s;
+            }
+           
+            return f;
+
+        }
+
+    }
+}
---
+++ NetLibraryTestTests/AnyCalcTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetLibraryTest;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetLibraryTest.Tests
+{
+    [TestClass()]
+    public class AnyCalcTests
+    {
+        [TestMethod()]
+        public void AnyFigureTest_cicle_13()
+        {
+            // arrange
+            double a = 13;
+            double expected = 530.929158456675;
+
+            // act
+            AnyCalc x = new AnyCalc();
+            AnyCalc.Figure actual = x.AnyFigure(a);
+
+            //assert
+            Assert.AreEqual(expected, actual.area);
+        }
+
+        [TestMethod()]
+        public void AnyFigureTest_rect_4_9_6()
+        {
+            // arrange
+            double a = 4;
+            double b = 9;
+            double c = 6;
+            double expected = 9.5;
+
+            // act
+            AnyCalc x = new AnyCalc();
+            AnyCalc.Figure actual = x.AnyFigure(a, b, c);
+
+            //assert
+            Assert.AreEqual(expected, actual.area);
+        }
+    }
+}
